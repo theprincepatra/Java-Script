303b50a3b0ab313ab2147e9018246d88b7d7a4d2
Prince Patra <princepatra9951@gmail.com>
2025-08-12 14:21:13 +0530

    Create backtick.js

---
+++ backtick.js
@@ -0,0 +1,8 @@
+console.log("~~~~~~~~~~~~~~~~~~~~~BACKTICK.JS")
+let b1 = `Prince`
+let b2 = `TPP`
+let sentence1 = `This is ${b1}`
+console.log(sentence1)
+//can written with aphostophy!
+let sentence2 = `This "is" ${b2}`
+console.log(sentence2)
