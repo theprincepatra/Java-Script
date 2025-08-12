beb5e69f23f9e769d68ac9bbdf335d00e8fe47ec
Prince Patra <princepatra9951@gmail.com>
2025-08-12 14:20:38 +0530

    Create 3) Expression&Condition.js

---
+++ 3) Expression&Condition.js
@@ -0,0 +1,9 @@
+// fragment of code that produces a value called Expression
+/* types of operator
+    1)Arithmetic Operator: +, -, *, **, /(here it gives quotient with decimal), %, ++, --
+    2)Assignment opewrator: =, +=, -=, *=, /=, %=, **=
+    3)Comparision Operator: ==(equal value), !=(equal value), ===(equal value & type), !==(neither for value nor for the type),>,<,>=,<=,?
+    4)Logical Operator: &&(true if both value is true), ||(true if either of any value is true), !(change the boolean value)
+    5)Teranry Operator: syntax: condition?exp1:exp2(if conditon get true ex1 get executesd else exp2 get executed)
+*/
+// for condition statement js has 3 forms: 1)if statement 2)if else statement 3)if- else if statement
