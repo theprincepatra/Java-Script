0914cf59cc158dc7b4fdef0f70f42b0f136eb123
Prince Patra <princepatra9951@gmail.com>
2025-08-20 09:22:15 +0530

    Create 7)ArrayMethods.js

---
+++ 7)ArrayMethods.js
@@ -0,0 +1,19 @@
+console.log("~~~~~~~~~~~~~~~~~~~~~ARRAYMETHODS.JS")
+let z = ["tpp", 7, false]
+let l = ["tap", 99]
+let m = ["tip"]
+let n =[7, 9, 1, 56, 22, 9, 45, 12, 96, 58, 73]
+let str = a.toString()
+console.log(Array.from(123))
+console.log(z.concat(l))
+console.log(l.shift())
+console.log(z.unshift(1))
+console.log(delete m[0])
+console.log(n.sort)
+console.log(n.reverse)
+console.log(n.splice(2, 3, 1111, 2222, 3333))
+console.log(n) //to know what are added from the above line
+console.log(l.join(" \'GREAT\' "))
+console.log(typeof z, typeof str)
+console.log(m.push(12))
+console.log(l.pop())
