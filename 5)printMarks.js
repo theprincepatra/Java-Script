69d93640ac2cc5bdf3c8e7d99ff8b056b64b3804
Prince Patra <princepatra9951@gmail.com>
2025-08-20 09:20:58 +0530

    Create 5)printMarks.js

---
+++ 5)printMarks.js
@@ -0,0 +1,9 @@
+console.log("~~~~~~~~~~~~~~~~~~~~~PRINTMARKS.JS")
+let marks = {
+    TPP: 99,
+    "BHUSKA BHAI": 8,
+    KHORA: 9
+}
+for(let key in marks){
+    console.log(`Makrs of ${key} is ${marks[key]}`)
+}
