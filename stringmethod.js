5deb091df8ffcd9c5944f2a77bba6f7010783024
Prince Patra <princepatra9951@gmail.com>
2025-08-20 09:21:22 +0530

    Create stringmethod.js

---
+++ stringmethod.js
@@ -0,0 +1,14 @@
+console.log("_______________________________________")
+let person = "TPP"
+console.log(person.length);
+console.log(person.slice(2, 4));
+console.log(person.replace("T", "Z"));
+let person2= "CapitalLetter"
+let person3= "SmallLetter"
+console.log(person2.concat(person3, "->DONE"));
+let space = "    t    p    p    "
+console.log(space.trim())
+let sentence = "Hi, this is PRINCE"
+console.log(sentence.includes("---"))
+console.log(sentence.includes("PRINCE"))
+console.log("_______________________________________")
