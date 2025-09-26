46d318d7af2d23f8a1460f38f9f86da38c20a594
Prince Patra <princepatra9951@gmail.com>
2025-09-26 17:31:48 +0530

    Create 02)Datatypes&Object.js

---
+++ 02)Datatypes&Object.js
@@ -0,0 +1,19 @@
+// the set of basic primitive dataypes in js: Null, Number, Symbol, String, Boolean, BigInt, Undefined
+//example for all the primitive datatypes:
+let q = null
+let r = 345
+let s = true
+let d = BigInt("567")
+let e = "Harry"
+let f = Symbol("I am a nice symmbol")
+let g = undefined
+
+// now for the object also called the non-primitve datatypes
+// example:
+const name = {
+    "TPP": 99,
+    "TAP": 89,
+    "TIP": 78
+}
+/* to acces any key from the object: */ name["TAP"]
+//In key part we have to give the quotes when it has a special; character, spcaes in btw or number:
