dc51bc44ac8d5b279d5ec28b744211140f2f8547
Prince Patra <princepatra9951@gmail.com>
2025-09-26 17:30:33 +0530

    Create 01)Var&itsRule.js

---
+++ 01)Var&itsRule.js
@@ -0,0 +1,11 @@
+//3 types of keyword used here--  1. var 2. let 3. const
+var a = 1  //change it later; declare it again; with a function we can use it anywhere it's declared
+let b = 2  //change it later; can't declare again; work inside the block you declared
+const c = 3  //can't change; can't declare; work inside the block you declared
+//can be initialized, let& consta are not neccessary to be initialized
+// const keywords must be initialized, let and const are not neccessary to be initialized
+
+//Rules forn choosing variavble name
+// 1) Letters, digit, underscore and dollar($) sign allowed
+// 2) Must begin with a letter, $ or _
+// 3) Javascript reserved words cant be used as var name
